49d594edd4ef74e726d91cafd855beb3e797b3d6
joaobecke <140556082+joaobecke@users.noreply.github.com>
2023-11-13 18:34:09 -0300

    aulas
    aulas

---
+++ secçao2/2_argumentos.py
@@ -0,0 +1,11 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+
+def soma(x, y):
+    print(f'{x=} + y={y}', x + y)
+
+
+soma(1, 2)
