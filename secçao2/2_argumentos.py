af666f5b105db0d9940ee5af31d2b01782904e76
joaobecke <140556082+joaobecke@users.noreply.github.com>
2023-11-14 17:34:31 -0300

    aulas

--- secçao2/2_argumentos.py
+++ secçao2/2_argumentos.py
@@ -2,10 +2,13 @@
 Argumentos nomeados e não nomeados em funções Python
 Argumento nomeado tem nome com sinal de igual
 Argumento não nomeado recebe apenas o argumento (valor)
+Refatorar: editar seu codigo.
 """
 
-def soma(x, y):
-    print(f'{x=} + y={y}', x + y)
+def soma(x, y, z=None):
+    if z is not None:
+     print(x + y + z)
+    else:
+        print (x + y)
 
-
-soma(1, 2)
+soma(1, 2, 4)
